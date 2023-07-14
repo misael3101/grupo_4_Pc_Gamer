0b381e804231aba8d1b299e85e296e98a85bf870
maxi-perez1 <perezmaxi745@gmail.com>
2023-07-13 21:10:07 -0300

    cree el archivo app y puse las rutas

---
+++ app.js
@@ -0,0 +1,13 @@
+//requerir los paquetes
+
+const express = require("express");
+const app = express();
+
+const pach = require("path");
+
+//poner a escuchar al servidor 
+
+app.listen(3555, () => console.log("servidor corriendo"))
+
+
+// manejor de rutas
