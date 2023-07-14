7ace695f946b9420234eb890e0678597bd4f3dc4
maxi-perez1 <perezmaxi745@gmail.com>
2023-07-14 08:52:12 -0300

    arme y puse el link de inicio de sesion y carrito de compras

--- src/app.js
+++ src/app.js
@@ -6,18 +6,30 @@ const app = express();
 
 const pach = require("path");
 
+//declarar los recursos estaticos (img, css, etc)
+
+app.use(express.static("public"))
+
 //poner a escuchar al servidor 
 
-app.listen(3555, () => console.log("servidor corriendo"))
+app.listen(3555, () => console.log("servidor corriendo  http://localhost:3555"))
 
 
 // manejor de rutas
+
 
 // ruta Raiz
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 })
-//
-app.get("/pato", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+//carrito de compras
+
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
+})
+
+// inicio de sesion para el usuario
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
 })
