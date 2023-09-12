f6e1c58196862e61281d093dcd8fb11236eb4fbb
misael3101 <torres.31012407@gmail.com>
2023-09-12 18:43:36 -0300

    agrege cosas

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,3 +1,4 @@
+const { Console } = require('console');
 const fs = require('fs');
 const path = require('path');
 
@@ -18,10 +19,96 @@ let controlador = {
     create: (req, res) => {
 		res.render('crearproducto')
 	},
+    prossCreate: (req, res) => {
 
+		data = req.body
+
+        const productos = JSON.parse(fs.readFileSync(direccionDataProducto, 'utf-8'));
+
+        const newProduct = {
+			id: productos[productos.length - 1].id + 1,
+			name: data.name,
+			price: data.price,
+			discount: data.discount,
+			category: data.category,
+			description: data.description,
+			image: "PCG-1.png"
+		   }
+
+           productos.push(newProduct);
+
+           fs.writeFileSync(direccionDataProducto, JSON.stringify(productos, null, " "))
+           
+           res.redirect("/");
+    },
+        
     edit: (req, res) => {
-		res.render('editarproducto')
-	},
+            const productos = JSON.parse(fs.readFileSync(direccionDataProducto, 'utf-8'));
+            const id = req.params.id;
+          
+            //buscamos el producto
+            let productoAEditar = productos.find(producto => {
+              return producto.id == id
+            });
+          
+            //inicializamos la variable productoAEditar con el resultado de la función find()
+            productoAEditar = productoAEditar;
+          
+            res.render('editarproducto', {productoAEditar});
+    },
+
+    edicion: (req, res) => {
+        const productos = JSON.parse(fs.readFileSync(direccionDataProducto, 'utf-8'));
+      
+        const id = req.params.id;
+      
+        let productToEdit = productos.find(product => {
+          return id == product.id
+        });
+      
+        let editedProduct = {
+          id: req.params.id,
+          name: req.body.name,
+          description: req.body.description,
+          price: req.body.price,
+          discount: req.body.discount,
+          image: productToEdit.image,
+          category: req.body.category
+        }
+      
+        // Buscando la posicion en el array del producto a editar
+        let indice = productos.findIndex(product => {
+          return product.id == req.params.id
+        });
+      
+        productos[indice] = editedProduct;
+      
+        fs.writeFileSync(direccionDataProducto, JSON.stringify(productos, null, " "));
+      
+        res.redirect('/')
+      },
+
+      destroy : (req, res) => {
+		
+		//obtenemos el id
+		const id = req.params.id;
+
+		//traemos el archivo
+		const productos = JSON.parse(fs.readFileSync(direccionDataProducto, 'utf-8'));
+
+		//eliminamos el objeto literal con el id correspondiente
+		let productosFiltrados = productos.filter(product => {
+			return product.id != id
+		});
+
+		//sobrescribimos el JSON
+		fs.writeFileSync(direccionDataProducto, JSON.stringify(productosFiltrados, null, " "));
+
+		//redireccionamos
+		res.redirect('/')
+	}
+
+
 }
 
-module.exports = controlador;
+module.exports = controlador
--- src/routers/productos.js
+++ src/routers/productos.js
@@ -1,14 +1,22 @@
 const express = require("express");;
 const app = express();
 const router = express.Router()
+const bodyParser = require('body-parser');
 
 const productControlador = require('../controllers/productController');
 
 router.get('/detalleP/:id', productControlador.productDetails);
 
-router.get('/create-product/', productControlador.create);
+//crear producto
+router.get('/create-product/', productControlador.create); 
+router.post('/create-product/', productControlador.prossCreate); 
 
-router.get('/edit-product/:id' , productControlador.edit)
+//editar producto
+router.get('/edit-product/:id' , productControlador.edit);
+router.post('/edit-product/:id' , productControlador.edicion, bodyParser.json())
+
+//eliminar
+router.post('/delete/:id', productControlador.destroy);
 
 
 
