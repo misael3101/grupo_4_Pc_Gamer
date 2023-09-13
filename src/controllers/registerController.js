0fa6ae12ff77843e1991009c17b40816def0b461
Lucas450B <lucasvazquez828@gmail.com>
2023-09-13 02:50:25 -0300

    se agrego el json de usuarios

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,7 +1,13 @@
+const { Console } = require('console');
+const fs = require('fs');
+const path = require('path');
+
+let direccionDataUser = path.join(__dirname, '../data/dataUsuario.json')
+
 let controlador = {
     register: function(req, res) {
         res.render('register');
-    }
+    },
 }
 
 module.exports = controlador;
