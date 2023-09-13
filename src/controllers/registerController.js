1f12a453665cf5c937154a394312b34b3cf8b53e
Lucas450B <lucasvazquez828@gmail.com>
2023-09-13 02:52:05 -0300

    cambie algunas cosas

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,9 +1,3 @@
-const { Console } = require('console');
-const fs = require('fs');
-const path = require('path');
-
-let direccionDataUser = path.join(__dirname, '../data/dataUsuario.json')
-
 let controlador = {
     register: function(req, res) {
         res.render('register');
